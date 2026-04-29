oldkingOK/kwm

commit b8180cc88d3e4c20635607d7ee290c0ea267b30b

fix: early return if geometry undefined

src/kwm/window.zig
@@ -757,6 +757,11 @@ pub fn apply_rules(self: *Self) void {
 pub fn manage(self: *Self) void {
     log.debug("<{*}> managing, propose dimensions: (width: {}, height: {})", .{ self, self.width, self.height });
 
+    if (self.geometry_undefined) {
+        self.rwm_window.proposeDimensions(0, 0);
+        return;
+    }
+
     const width, const height = blk: {
         const config = Config.get();
 

commit 5f56af5b70704314b919683ea96fe666808f2446

fix: check dimension rules before setting geometry undefined
This fixes the regression introduced by commit
b8180cc88d3e4c20635607d7ee290c0ea267b30b where windows dimensions from
rules are not respected.

src/kwm/window.zig
@@ -590,7 +590,14 @@ pub fn handle_events(self: *Self) void {
                 }
 
                 if (self.floating or self.output == null or self.output.?.current_layout() == .float) {
-                    self.geometry_undefined = true;
+                    if (self.width > 0 and self.height > 0) {
+                        self.geometry_undefined = false;
+                        if (self.output != null) {
+                            self.center();
+                        }
+                    } else {
+                        self.geometry_undefined = true;
+                    }
                 }
 
                 if (self.is_terminal) {